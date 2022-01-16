bb39c4108c9d12ba100d4652fe0fc827693e0039
emm5847 <emm584759@gmail.com>
2022-01-17 01:28:05 +0800

    Create image

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#Install mongodb
+#
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+apt-get install apt-transport-https ca-certificates && sudo apt-get update && sudo apt-get install -y mongodb-org
+
+systemctl start mongod
+systemctl enable mongod
+
+systemctl is-enabled mongod && systemctl is-active mongod
+
+if [[ $? == 0 ]]
+then
+    echo "Mongodb is successfully started"
+else
+    echo "Mongod istallation error"
+fi
