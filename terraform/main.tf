58e4def801be6345e68e59bde780af014a945140
emm5847 <emm584759@gmail.com>
2022-01-27 02:31:24 +0800

    Add new files

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,34 +7,51 @@ terraform {
 }
 
 provider "yandex" {
-  token = "AQAAAABb-fP9AATuwZ7v28HLNUTZoDPvOL3aCq4"
-  cloud_id = "b1g35okphf23m1l8pb6k"
-  folder_id = "b1gsn46bhh43i65o0lio"
-  zone = "ru-central1-a"
+  cloud_id = var.cloud_id
+  folder_id = var.folder_id
+  zone = var.zone
+  service_account_key_file = var.service_account_key_file
 }
 
 resource "yandex_compute_instance" "app" {
   name        = "reddit-app"
 
   resources {
-    cores  = 1
+    core_fraction = 5
+    cores  = 2
     memory = 2
   }
 
   boot_disk {
     initialize_params {
-      image_id = "fd8h24coiv2imnl8q7rj"
+      image_id = var.image_id
     }
   }
 
   network_interface {
-    subnet_id = "e9bgtqs5gjstf98pbeli"
+    subnet_id = var.subnet_id
     nat = true 
   }
 
   metadata = {
-    foo      = "bar"
-    ssh-keys = "ubuntu:${file("~/.ssh/appuser.pub")}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type = "ssh"
+    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user = "ubuntu"
+    agent = false
+    private_key = file("~/.ssh/appuser")
+  }
+
+  provisioner "file" {
+    source = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
   }
 }
 
---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable cloud_id{
+  description = "Cloud"
+  default = "b1g35okphf23m1l8pb6k"
+}
+variable folder_id {
+  description = "Folder"
+  default = "b1gsn46bhh43i65o0lio"
+}
+variable zone {
+  description = "Zone"
+  default = "ru-central1-a"
+}
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+  default = "/home/home/.ssh/appuser.pub"
+}
+variable image_id {
+  description = "Disk image"
+  default = "fd8ramkfn4p56qp0f9l6"
+}
+variable subnet_id{
+  description = "Subnet"
+  default = "e9bgtqs5gjstf98pbeli"
+}
+variable service_account_key_file{
+  description = "key .json"
+  default = "/home/home/Documents/DevOPS/HomeWork/key.json"
+}
+
