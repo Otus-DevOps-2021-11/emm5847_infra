848b2619c0634aa59a2e5545480ccd85bf7bc9b5
emm5847 <emm584759@gmail.com>
2022-01-30 23:56:04 +0800

    Changed main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#}
 
 provider "yandex" {
   cloud_id                 = var.cloud_id
