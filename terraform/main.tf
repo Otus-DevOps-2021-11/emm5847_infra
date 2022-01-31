e74938b10503a944139b3de8e648d6ca42a2aea2
emm5847 <emm584759@gmail.com>
2022-01-31 21:48:38 +0800

    Yandex provide 0.70.0, terraform 0.13.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
 provider "yandex" {
-  version                  = "~> 0.43"
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
