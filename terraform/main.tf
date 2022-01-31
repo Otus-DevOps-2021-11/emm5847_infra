acca75ffb0ba2c8f5344522bd13459d85fb0b265
emm5847 <emm584759@gmail.com>
2022-02-01 01:42:47 +0800

    yandex version = 0.58

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "yandex" {
-  version                  = "~> 0.60"
+  version                  = "~> 0.58"
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
