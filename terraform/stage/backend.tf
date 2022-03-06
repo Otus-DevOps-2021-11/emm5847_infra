74fe14bd714bfc98da29c82cb2e61f7bd9fe6f51
emm5847 <emm584759@gmail.com>
2022-03-06 16:45:09 +0800

    Edited files

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -4,8 +4,8 @@ terraform {
     bucket     = "tf-bucket-11022022"
     region     = "us-east-1"
     key        = "terraform.tfstate"
-    access_key = "6d__pBkuPXXzB42vbY_-"
-    secret_key = "p8SjS1_eko1NlvbHFKEmfTTxnFX15G_uorzpLmAO"
+    access_key = "PrflxzoQasepm"
+    secret_key = "AsdeFpeoqlfdszldjeo"
 
     skip_region_validation      = true
     skip_credentials_validation = true
